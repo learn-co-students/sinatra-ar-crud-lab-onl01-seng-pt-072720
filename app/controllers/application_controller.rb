b568617b959dabf5d3ab0de1ea884234e7668831
Kelin Field <kfield111@hotmail.com>
2020-10-31 03:45:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,47 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    data = params
+    @article = Article.create(data)
+    redirect to "/articles/#{@article.id}"
+  end
+
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+
+  get '/articles/:id'  do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @new_data = params[:article]
+    @article.update(@new_data)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
+
 end
