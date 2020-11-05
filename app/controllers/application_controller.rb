679e678b7c044d0aa23aadbc837ca6c69dd8928d
aarongoen <aarongoen2.0@gmail.com>
2020-11-05 11:46:06 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,28 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+    
   get '/' do
+
+  end
+
+  get '/articles/new' do
+    @article = Article.new
+    erb :'/articles/new'
+  end
+
+  post '/articles' do
+    Article.create(params)
+    redirect '/articles'
+  end
+
+  get '/articles' do
+    @articles = Article.all 
+    erb :'/articles/index'
+  end
+
+  get '/articles/:id' do
+    @article = @articles.find(params[:id])
+    erb :'/articles/show'
   end
 end
